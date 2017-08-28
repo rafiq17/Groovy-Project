e12ecc3f25cf4b9b3169f1568d7ae8b3effbc924
Mehaboob <mohamed.r.mehaboob@accenture.com>
2017-08-28 15:31:09 +0530

    Groovy Project

---
+++ src/test/groovy/Demo/HelperTest.groovy
@@ -0,0 +1,18 @@
+//
+// Generated from archetype; please customize.
+//
+
+package Demo
+
+import groovy.util.GroovyTestCase
+
+/**
+ * Tests for the {@link Helper} class.
+ */
+class HelperTest
+    extends GroovyTestCase
+{
+    void testHelp() {
+        new Helper().help(new Example())
+    }
+}
